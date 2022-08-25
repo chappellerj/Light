b1d5288a1c75d30bc55115c9611fbee52f5e289d
Ryan Chappelle <77121182+chappellerj@users.noreply.github.com>
2022-08-25 08:41:44 -0700

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Ryan Chappelle on 8/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var lightOn = true
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        updateUI()
+    }
+    
+    fileprivate func updateUI() {
+        view.backgroundColor = lightOn ? .white : .black
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+        updateUI()
+    }
+}
+
